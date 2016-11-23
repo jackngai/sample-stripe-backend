48f03916835ed166aa740a7d947831f9e90f46b0
Aleksandr Girdo <sonner@hell.local>
2016-11-23 16:59:15 +0200

    Add stripe endpoints

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,31 @@ class UsersController < ApplicationController
     user.save
     render json: user, status: :created
   end
+
+  def customer
+    user = User.find(params[:id])
+    customer = Stripe::Customer.retrieve(user.customerId)
+    render json: customer
+  end
+
+  def customer_sources
+    begin
+      user = User.find(params[:id])
+      customer = Stripe::Customer.retrieve(user.customerId)
+      customer.sources.create(source: params[:source])
+    rescue Stripe::StripeError => e
+      render json: {error: "Error retrieving customer: #{e.message}"}, status: 402
+    end
+  end
+
+  def customer_default_source
+    begin
+      user = User.find(params[:id])
+      customer = Stripe::Customer.retrieve(user.customerId)
+      customer.default_source = params[:default_source]
+      customer.save
+    rescue Stripe::StripeError => e
+      render json: {error: "Error retrieving customer: #{e.message}"}, status: 402
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'users', to: "users#index"
 
   post 'users', to: "users#new"
+  get 'users/:id/customer', to: "users#customer"
+  post 'users/:id/customer/sources', to: "users#customer_sources"
+  post 'users/:id/customer/default_source', to: "users#customer_default_source"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
